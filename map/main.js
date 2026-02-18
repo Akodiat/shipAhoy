9bb08d2bcb41c4cd416df12cc0f13e6f2caabcae
Joakim Bohlin <joakim.bohlin@chalmers.se>
2026-02-18 10:53:07 +0100

    Add ship type selector to map view

--- map/main.js
+++ map/main.js
@@ -1,31 +1,68 @@
 import {parseCSVPath} from "./csv.js";
 
-const {DeckGL, ScatterplotLayer, H3HexagonLayer} = deck;
+const {DeckGL, H3HexagonLayer} = deck;
 
-const data = await parseCSVPath("cargo.csv");
-data.forEach(d=>d.value = parseFloat(d.value));
+const mapContainer = document.getElementById("mapContainer");
 
-const maxVal = Math.max(...data.map(v=>v.value));
+const shipTypes = ["Cargo", "Passenger", "Tanker"];
 
-new DeckGL({
-    container: document.body,
-    width: '100vw',
-    height: '100vh',
+const data = new Map();
+for (const t of shipTypes) {
+    // Read csv file
+    const d = await parseCSVPath(`density${t}.csv`);
+    // Parse values as floats
+    d.forEach(d=>d.value = parseFloat(d.value));
+
+    data.set(t, d);
+}
+
+// Calculate max count for all ship types
+const maxVal = Math.max(...[...data.values()].flatMap(s=>s).map(s=>s.value));
+
+const deckGL = new DeckGL({
+    container: mapContainer,
     mapStyle: 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json',
     initialViewState: {
         longitude: 11.126,
         latitude: 55.244,
         zoom: 4
     },
-    controller: true,
-    layers: [
-        new H3HexagonLayer({
-            data: data,
-            extruded: false,
-            getHexagon: d => d.id,
-            opacity: 0.5,
-            filled: true,
-            getFillColor: d => [255, (1 - d.value / maxVal) * 255, 0]
-        })
-    ]
+    controller: true
 });
+
+/**
+ * Draw a provided ship type layer
+ * @param {string} shipType
+ */
+function renderLayer(shipType) {
+    const layer = new H3HexagonLayer({
+        id: shipType,
+        data: data.get(shipType),
+        extruded: false,
+        getHexagon: d => d.id,
+        opacity: 0.5,
+        filled: true,
+        getFillColor: d => [255, (1 - Math.log(d.value) / Math.log(maxVal)) * 255, 0]
+    });
+    deckGL.setProps({
+        layers: [layer]
+    });
+}
+
+/**
+ * Get selection from radio buttons
+ * @returns {string} Selected ship type
+ */
+function getShipType() {
+    return document.querySelector('input[name="shipType"]:checked').id;
+}
+
+// Draw currently selected ship type
+renderLayer(getShipType());
+
+// Update layes when selected ship type changes
+document.querySelectorAll('input[name="shipType"]').forEach(
+    e=>e.addEventListener("change", ()=>{
+        renderLayer(getShipType());
+    })
+);
